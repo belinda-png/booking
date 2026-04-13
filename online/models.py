ac4f76f9c802c7cd23b15214b5f20ab51ccb30ac
Mukhaye Belinda <namuzbelinda@gmail.com>
2026-04-13 19:56:05 +0300

    added class users

---
+++ online/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ("user", "User"),
+        ("vendor", "Vendor"),
+        ("admin", "Admin"),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="user")
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(unique=True)
+
+class Vendor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=255)
+    description = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.business_name
+    
+class Destination(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
