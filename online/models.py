0de76e7781329ff8090814c5e93a84cc28fa75b6
Mukhaye Belinda <namuzbelinda@gmail.com>
2026-04-13 19:58:54 +0300

    added list models

--- online/models.py
+++ online/models.py
@@ -29,3 +29,20 @@ class Destination(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Listing(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Destination, on_delete=models.SET_NULL, null=True)
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price_per_person = models.DecimalField(max_digits=10, decimal_places=2)
+
+    location = models.CharField(max_length=255)
+    duration_days = models.IntegerField(default=1)
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
