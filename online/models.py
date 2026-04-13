e984bf8a8b1d802915f45472009f75f0c21da0c0
Mukhaye Belinda <namuzbelinda@gmail.com>
2026-04-13 20:07:34 +0300

    added payment menthod

--- online/models.py
+++ online/models.py
@@ -78,3 +78,18 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.listing.title}"
+
+class Payment(models.Model):
+
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("successful", "Successful"),
+        ("failed", "Failed"),
+    ]
+
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    method = models.CharField(max_length=50)  # e.g. "mobile_money", "card"
+    transaction_id = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
