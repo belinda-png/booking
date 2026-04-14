565836db479cb9b96649a2045b2514e0516cf9ef
Mukhaye Belinda <namuzbelinda@gmail.com>
2026-04-14 17:04:58 +0300

    created view forr authication

--- online/views.py
+++ online/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import *
+from .serializers import *
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+class IsVendor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "vendor"
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "admin"
