fe2e6fc10863612d20deb0ee5fd8d57938a375da
belinda-png <namuzbelinda@gmail.com>
2026-09-10 22:23:14 +0300

    Added fights in the urls

--- online/urls.py
+++ online/urls.py
@@ -16,70 +16,48 @@ from .views import (
 
 
 router = DefaultRouter()
-
-# Users
 router.register(r"users", UserViewSet, basename="users")
-
-# Vendors
 router.register(r"vendors", VendorViewSet, basename="vendors")
-
-# Destinations
 router.register(
     r"destinations",
     DestinationViewSet,
     basename="destinations"
 )
-
-# Hotels
 router.register(
     r"hotels",
     HotelViewSet,
     basename="hotels"
 )
-
-# Hotel Rooms
 router.register(
     r"hotel-rooms",
     HotelRoomViewSet,
     basename="hotel-rooms"
 )
-
-# Flights
 router.register(
     r"flights",
     FlightViewSet,
     basename="flights"
 )
-
-# Cars
 router.register(
     r"cars",
     CarViewSet,
     basename="cars"
 )
-
-# Tours
 router.register(
     r"tours",
     TourViewSet,
     basename="tours"
 )
-
-# Bookings
 router.register(
     r"bookings",
     BookingViewSet,
     basename="bookings"
 )
-
-# Payments
 router.register(
     r"payments",
     PaymentViewSet,
     basename="payments"
 )
-
-# Reviews
 router.register(
     r"reviews",
     ReviewViewSet,
