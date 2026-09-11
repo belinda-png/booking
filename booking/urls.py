7210057994b173acdbfd45b53087bd9703495ea1
belinda-png <namuzbelinda@gmail.com>
2026-09-11 17:57:20 +0300

    updated urls

--- booking/urls.py
+++ booking/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 from django.http import JsonResponse
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 def api_root(request):
@@ -30,16 +31,15 @@ def api_root(request):
         "schema": "/api/schema/",
     })
 
-
 urlpatterns = [
     path("", api_root, name="api-root"),
-
     path("admin/", admin.site.urls),
 
-    # YOUR API
     path("api/v1/", include(("online.urls", "online"), namespace="v1")),
 
-    # DOCS
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
