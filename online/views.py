320127032a2e4f42e0f3f552cf0c4f8c063d7084
belinda-png <namuzbelinda@gmail.com>
2026-09-14 21:36:03 +0300

    modified changes

--- online/views.py
+++ online/views.py
@@ -6,6 +6,7 @@ from rest_framework import request, viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from .models import (
     User,
