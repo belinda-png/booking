b0177b4af7969506751d6259f2d4a539e2210aad
belinda-png <namuzbelinda@gmail.com>
2026-09-16 16:36:11 +0300

    modified the changes

--- frontend/src/components/AuthPage.jsx
+++ frontend/src/components/AuthPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { auth, googleProvider, db } from "../firebase"
+// import { auth, googleProvider, db } from "../firebase"
 
 import {
   createUserWithEmailAndPassword,
