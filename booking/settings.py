fa1eb3bcfe2c5f7000e55e402aad269b1e100dce
belinda-png <namuzbelinda@gmail.com>
2026-09-18 20:28:12 +0300

    modified changes

--- booking/settings.py
+++ booking/settings.py
@@ -37,8 +37,12 @@ SECRET_KEY = 'django-insecure-e+-x@ihgfpc2d_l1&%2u(v6h0-*p_bio$v^t8!ojja8wm0%^(b
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'booking-app-production.up.railway.app', 'tourism-booking.up.railway.app']
+ALLOWED_HOSTS = [
+    'tourism-booking.up.railway.app',
+    '.railway.app',  # allows any Railway subdomain, useful if it changes
+    'localhost',
+    '127.0.0.1',
+]
 
 AUTH_USER_MODEL = 'online.User'
 GOOGLE_CLIENT_ID = os.environ.get(
