ee29f4c0e0070b5dd2df2c17cf038c11aa7d73eb
belinda-png <namuzbelinda@gmail.com>
2026-09-20 22:55:17 +0300

    modified changes

--- online/serializers.py
+++ online/serializers.py
@@ -18,25 +18,6 @@ from .models import (
 # =========================
 # USER
 # =========================
-class GoogleAuthSerializer(serializers.Serializer):
-    credential = serializers.CharField()
-
-
-class GoogleUserSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    email = serializers.EmailField()
-    name = serializers.CharField()
-    picture = serializers.CharField(
-        allow_null=True,
-        required=False
-    )
-    created = serializers.BooleanField()
-
-
-class GoogleAuthResponseSerializer(serializers.Serializer):
-    access = serializers.CharField()
-    refresh = serializers.CharField()
-    user = GoogleUserSerializer()
 
 class UserSerializer(serializers.ModelSerializer):
 
